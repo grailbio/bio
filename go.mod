grailbio/base

grailbio: initialize go.mod files
Summary:
This is simply the result of `go mod init`.

Versioned depedency requirements will come in a later revision.

Test Plan:
`go build grail.com/...` works when using Go modules with `replace`
clauses to local copies in grail.com.

Reviewers: marius, ysaito

Reviewed By: marius, ysaito

Subscribers: pgopal

Maniphest Tasks: T14065

Differential Revision: https://phabricator.grailbio.com/D23520

fbshipit-source-id: bf8b18f

go.mod
@@ -1,0 +1,1 @@
+module github.com/grailbio/base

grailbio/bio

go.mod
@@ -1,0 +1,1 @@
+module github.com/grailbio/bio