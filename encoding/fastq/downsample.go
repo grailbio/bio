c352f3104b030b8736534caf2cf6202bba881982
Yaz Saito <ysaito@grailbio.com>
2019-02-16 05:48:45 +0000

    reflow/methylation: add downsample by number of reads
    Summary: Ref T14517.
    
    Test Plan: Added unit tests.
    
    Reviewers: escott, kshashidhar
    
    Reviewed By: escott, kshashidhar
    
    Maniphest Tasks: T14517
    
    Differential Revision: https://phabricator.grailbio.com/D25180
    
    fbshipit-source-id: b6cdb56

--- encoding/fastq/downsample.go
+++ encoding/fastq/downsample.go
@@ -3,48 +3,181 @@ package fastq
 import (
 	"bufio"
 	"bytes"
+	"context"
+	"fmt"
 	"io"
 	"math/rand"
 
-	"github.com/pkg/errors"
+	"github.com/grailbio/base/errors"
+	"github.com/grailbio/base/file"
+	"github.com/klauspost/compress/gzip"
 )
 
-const (
-	linesPerRead = 4
-)
+const linesPerRead = 4
 
-// Downsample writes read pairs from r1In and r2In to r1Out and r2Out. Read pairs will be randomly
-// selected for inclusion in the output at the given sampling rate.
-func Downsample(rate float64, r1In, r2In io.Reader, r1Out, r2Out io.Writer) error {
-	if rate < 0.0 || rate > 1.0 {
-		return errors.New("rate must be between 0 and 1 (inclusive)")
+// FileHandle captures state needed for reading a fastq file.  Thread
+// compatible.
+type fileHandle struct {
+	path string
+	f    file.File
+	r    io.ReadCloser // gzip or plaintext reader.
+	errp *errors.Once  // accumulates any error encountered by the file.
+}
+
+func (fh *fileHandle) seek(ctx context.Context, off int64) {
+	n, err := fh.f.Reader(ctx).Seek(off, io.SeekStart)
+	if err != nil {
+		fh.errp.Set(err)
+		return
 	}
+	if n != off {
+		panic(fh)
+	}
+}
+
+func (fh *fileHandle) reader(ctx context.Context) io.ReadCloser {
+	if fh.f == nil || fh.r != nil {
+		panic("multiple calls to reader")
+	}
+	r := fh.f.Reader(ctx)
+	// TODO(saito) Don't hardcode gzip, instead autodetect on magic prefix
+	// bytes. That would suffice for fastq.
+	var err error
+	if fh.r, err = gzip.NewReader(r); err != nil {
+		fh.errp.Set(err)
+		fh.r = file.NewError(err)
+	}
+	return fh.r
+}
+
+func (fh *fileHandle) close(ctx context.Context) {
+	if fh.r != nil {
+		if err := fh.r.Close(); err != nil {
+			fh.errp.Set(errors.E(err, "gzip close", fh.path))
+		}
+	}
+	if fh.f != nil {
+		if err := fh.f.Close(ctx); err != nil {
+			fh.errp.Set(errors.E(err, "close", fh.path))
+		}
+	}
+}
+
+func newFileHandle(ctx context.Context, path string, errp *errors.Once) *fileHandle {
+	fh := &fileHandle{path: path, errp: errp}
+	var err error
+	fh.f, err = file.Open(ctx, path)
+	errp.Set(err)
+	return fh
+}
+
+func doDownsample(ctx context.Context, rate float64, fh1, fh2 *fileHandle, r1Out, r2Out io.Writer, errp *errors.Once) {
 	random := rand.New(rand.NewSource(0))
-	r1Scanner := bufio.NewScanner(r1In)
-	r2Scanner := bufio.NewScanner(r2In)
+	r1Scanner := bufio.NewScanner(fh1.reader(ctx))
+	r2Scanner := bufio.NewScanner(fh2.reader(ctx))
 	for {
 		r1, r1Err := scanRead(r1Scanner)
 		if r1Err != nil && r1Err != io.EOF {
-			return errors.Wrap(r1Err, "error reading R1 input")
+			errp.Set(errors.E(r1Err, "read", fh1.path))
+			return
 		}
 		r2, r2Err := scanRead(r2Scanner)
 		if r2Err != nil && r2Err != io.EOF {
-			return errors.Wrap(r2Err, "error reading R2 input")
+			errp.Set(errors.E(r2Err, "read", fh2.path))
+			return
 		}
 		if r1Err == io.EOF && r2Err == io.EOF {
 			// Both readers ended after the same number of reads, as expected.
-			return nil
+			break
 		} else if r1Err == io.EOF {
-			return errors.New("more reads in R2 input than in R1 input")
+			errp.Set(errors.E("more reads in R2 input than in R1 input", fh1.path, fh2.path))
+			return
 		} else if r2Err == io.EOF {
-			return errors.New("more reads in R1 input than in R2 input")
+			errp.Set(errors.E("more reads in R1 input than in R2 input", fh1.path, fh2.path))
+			return
 		}
 		if random.Float64() < rate {
-			r1Out.Write(r1)
-			r2Out.Write(r2)
+			if _, err := r1Out.Write(r1); err != nil {
+				errp.Set(errors.E(err, "write R1"))
+				return
+			}
+			if _, err := r2Out.Write(r2); err != nil {
+				errp.Set(errors.E(err, "write R2"))
+				return
+			}
 		}
 	}
-	return nil
+}
+
+// Downsample writes read pairs from the two files to r1Out and r2Out. Read
+// pairs will be randomly selected for inclusion in the output at the given
+// sampling rate.
+func Downsample(ctx context.Context, rate float64, r1Path, r2Path string, r1Out, r2Out io.Writer) error {
+	if rate < 0.0 || rate > 1.0 {
+		return errors.New("rate must be between 0 and 1 (inclusive)")
+	}
+	e := errors.Once{}
+	fh1 := newFileHandle(ctx, r1Path, &e)
+	fh2 := newFileHandle(ctx, r2Path, &e)
+	if e.Err() == nil {
+		doDownsample(ctx, rate, fh1, fh2, r1Out, r2Out, &e)
+	}
+	return e.Err()
+}
+
+// DownsampleToCount writes read pairs from the two files to r1Out and
+// r2Out. Read pairs will be randomly selected for inclusion in the output to
+// downsample to the given count, approximately.
+func DownsampleToCount(ctx context.Context, count int64, r1Path, r2Path string, r1Out, r2Out io.Writer) (err error) {
+	if count <= 0 {
+		return errors.E("count must be >= 1")
+	}
+	e := errors.Once{}
+	defer func() { err = e.Err() }()
+
+	fh1 := newFileHandle(ctx, r1Path, &e)
+	defer fh1.close(ctx)
+	fh2 := newFileHandle(ctx, r2Path, &e)
+	defer fh2.close(ctx)
+	if e.Err() != nil {
+		return
+	}
+
+	// Translate the sample count to sampling rate by counting the # of reads in
+	// the the first 100KiB of the file, then estimating the # of bytes per read.
+	f1Stat, err := fh1.f.Stat(ctx)
+	if err != nil {
+		e.Set(err)
+		return
+	}
+	prefixLen := int64(100 << 10)
+	if prefixLen > f1Stat.Size() {
+		prefixLen = f1Stat.Size()
+	}
+	r1PrefixReader, err := gzip.NewReader(&io.LimitedReader{R: fh1.f.Reader(ctx), N: prefixLen})
+	if err != nil {
+		e.Set(err)
+		return
+	}
+	// Count the # of reads in the first 100KiB
+	scanner := bufio.NewScanner(r1PrefixReader)
+	nLine := 0
+	for scanner.Scan() {
+		nLine++
+	}
+	var (
+		nRead = nLine / linesPerRead
+		rate  = 1.0
+	)
+	if nRead > 0 {
+		_ = r1PrefixReader.Close() // this should fail with a checksum error.
+		// Approximate the # bytes per read, compressed
+		approxCompressedReadLen := float64(prefixLen) / float64(nRead)
+		rate = float64(count) * approxCompressedReadLen / float64(f1Stat.Size())
+	}
+	fh1.seek(ctx, 0)
+	doDownsample(ctx, rate, fh1, fh2, r1Out, r2Out, &e)
+	return
 }
 
 func scanRead(scanner *bufio.Scanner) ([]byte, error) {
@@ -59,7 +192,7 @@ func scanRead(scanner *bufio.Scanner) ([]byte, error) {
 			if scanner.Err() != nil {
 				return nil, scanner.Err()
 			}
-			return nil, errors.Errorf("too few lines in FASTQ record: want %d, got %d", linesPerRead, i)
+			return nil, fmt.Errorf("too few lines in FASTQ record: want %d, got %d", linesPerRead, i)
 		}
 		buffer.WriteString(scanner.Text())
 		buffer.WriteString("\n")
--- encoding/fastq/downsample_test.go
+++ encoding/fastq/downsample_test.go
@@ -2,13 +2,28 @@ package fastq_test
 
 import (
 	"bytes"
+	"compress/gzip"
+	"context"
 	"fmt"
-	"reflect"
+	"io/ioutil"
 	"strings"
 	"testing"
 
 	"github.com/grailbio/bio/encoding/fastq"
+	"github.com/grailbio/testutil"
+	"github.com/grailbio/testutil/assert"
+	"github.com/grailbio/testutil/expect"
 )
+
+func writeFile(t *testing.T, path string, data []string) {
+	buf := bytes.Buffer{}
+	gz := gzip.NewWriter(&buf)
+	for _, line := range data {
+		gz.Write([]byte(line + "\n"))
+	}
+	assert.NoError(t, gz.Close())
+	assert.NoError(t, ioutil.WriteFile(path, buf.Bytes(), 0600))
+}
 
 func TestDownsample(t *testing.T) {
 	tests := []struct {
@@ -76,27 +91,84 @@ func TestDownsample(t *testing.T) {
 			fmt.Errorf("error reading R2 input: too few lines in FASTQ record: want 4, got 2"),
 		},
 	}
-	for _, test := range tests {
-		var r1In, r2In, r1Out, r2Out bytes.Buffer
-		for _, line := range test.r1InLines {
-			r1In.WriteString(line + "\n")
-		}
-		for _, line := range test.r2InLines {
-			r2In.WriteString(line + "\n")
-		}
-		err := fastq.Downsample(test.rate, &r1In, &r2In, &r1Out, &r2Out)
-		if err == nil && test.err != nil {
-			t.Errorf("did not get expected error: %v", test.err)
-			continue
-		}
-		if err != nil && test.err == nil {
-			t.Errorf("got unexpected error: %v", err)
-			continue
-		}
-		if test.err == nil {
-			checkDownsampleOutput(t, test.r1OutLines, &r1Out)
-			checkDownsampleOutput(t, test.r2OutLines, &r2Out)
-		}
+
+	tempDir, cleanup := testutil.TempDir(t, "", "")
+	defer cleanup()
+	ctx := context.Background()
+	for idx, test := range tests {
+		t.Run(fmt.Sprint(idx), func(t *testing.T) {
+			r1Path := fmt.Sprintf("%s/%dr1.fastq", tempDir, idx)
+			r2Path := fmt.Sprintf("%s/%dr2.fastq", tempDir, idx)
+			writeFile(t, r1Path, test.r1InLines)
+			writeFile(t, r2Path, test.r2InLines)
+			var r1Out, r2Out bytes.Buffer
+			err := fastq.Downsample(ctx, test.rate, r1Path, r2Path, &r1Out, &r2Out)
+			if err == nil && test.err != nil {
+				t.Errorf("did not get expected error: %v", test.err)
+				return
+			}
+			if err != nil && test.err == nil {
+				t.Errorf("got unexpected error: %v", err)
+				return
+			}
+			if test.err == nil {
+				checkDownsampleOutput(t, test.r1OutLines, &r1Out)
+				checkDownsampleOutput(t, test.r2OutLines, &r2Out)
+			}
+		})
+	}
+}
+
+func TestDownsampleToCount(t *testing.T) {
+	tests := []struct {
+		count      int64
+		r1InLines  []string
+		r2InLines  []string
+		r1OutLines []string
+		r2OutLines []string
+		err        error
+	}{
+		{
+			2,
+			[]string{"a", "b", "c", "d", "e", "f", "g", "h"},
+			[]string{"i", "j", "k", "l", "m", "n", "o", "p"},
+			[]string{"a", "b", "c", "d", "e", "f", "g", "h"},
+			[]string{"i", "j", "k", "l", "m", "n", "o", "p"},
+			nil,
+		},
+		{
+			1,
+			[]string{"a", "b", "c", "d", "e", "f", "g", "h"},
+			[]string{"i", "j", "k", "l", "m", "n", "o", "p"},
+			[]string{"e", "f", "g", "h"},
+			[]string{"m", "n", "o", "p"},
+			nil,
+		},
+	}
+	tempDir, cleanup := testutil.TempDir(t, "", "")
+	defer cleanup()
+	ctx := context.Background()
+	for idx, test := range tests {
+		t.Run(fmt.Sprint(idx), func(t *testing.T) {
+			r1Path := fmt.Sprintf("%s/%dr1.fastq", tempDir, idx)
+			r2Path := fmt.Sprintf("%s/%dr2.fastq", tempDir, idx)
+			writeFile(t, r1Path, test.r1InLines)
+			writeFile(t, r2Path, test.r2InLines)
+			var r1Out, r2Out bytes.Buffer
+			err := fastq.DownsampleToCount(ctx, test.count, r1Path, r2Path, &r1Out, &r2Out)
+			if err == nil && test.err != nil {
+				t.Errorf("did not get expected error: %v", test.err)
+				return
+			}
+			if err != nil && test.err == nil {
+				t.Errorf("got unexpected error: %v", err)
+				return
+			}
+			if test.err == nil {
+				checkDownsampleOutput(t, test.r1OutLines, &r1Out)
+				checkDownsampleOutput(t, test.r2OutLines, &r2Out)
+			}
+		})
 	}
 }
 
@@ -106,10 +178,36 @@ func checkDownsampleOutput(t *testing.T, expected []string, actual *bytes.Buffer
 		// We need this special case due to the behavior of strings.Split().
 		actualLines = []string{}
 	}
-	if len(actualLines) != len(expected) {
-		t.Errorf("wrong number of output lines: want %d, got %d", len(expected), len(actualLines))
+	expect.EQ(t, actualLines, expected)
+}
+
+func TestDownsampleLarge(t *testing.T) {
+	const nRead = 57209 // The below two files contain 57209 reads each.
+	r1Path := testutil.GetFilePath("//reflow/modules/testdata/af4/test_1.fastq.gz")
+	r2Path := testutil.GetFilePath("//reflow/modules/testdata/af4/test_2.fastq.gz")
+	for _, count := range []int64{100, 1000, 10000} {
+		t.Run("count-"+fmt.Sprint(count), func(t *testing.T) {
+			t.Parallel()
+			var r1Out, r2Out bytes.Buffer
+			assert.NoError(t, fastq.DownsampleToCount(context.Background(), count, r1Path, r2Path, &r1Out, &r2Out))
+			nLine1 := bytes.Count(r1Out.Bytes(), []byte("\n")) / 4
+			nLine2 := bytes.Count(r2Out.Bytes(), []byte("\n")) / 4
+			expect.EQ(t, nLine1, nLine2)
+			expect.GE(t, nLine1, int(float64(count)*0.9))
+			expect.LE(t, nLine1, int(float64(count)*1.1))
+		})
 	}
-	if !reflect.DeepEqual(expected, actualLines) {
-		t.Errorf("wrong downsample output: want %v, got %v", expected, actualLines)
+
+	for _, rate := range []float64{0.001, 0.01, 0.1} {
+		t.Run("rate-"+fmt.Sprint(rate), func(t *testing.T) {
+			t.Parallel()
+			var r1Out, r2Out bytes.Buffer
+			assert.NoError(t, fastq.Downsample(context.Background(), rate, r1Path, r2Path, &r1Out, &r2Out))
+			nLine1 := bytes.Count(r1Out.Bytes(), []byte("\n")) / 4
+			nLine2 := bytes.Count(r2Out.Bytes(), []byte("\n")) / 4
+			expect.EQ(t, nLine1, nLine2)
+			expect.GE(t, nLine1, int(nRead*rate*0.9))
+			expect.LE(t, nLine1, int(nRead*rate*1.1))
+		})
 	}
 }
