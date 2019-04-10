c5a5df2f231e7bd6ea1adbbd01ae3a00c531fdbc
Christopher Chang <cchang@grailbio.com>
2019-04-09 18:15:09 -0700

    bio/encoding/bam: ClearAuxTags and GetStrand utility functions
    Summary:
    This moves a pair of formerly one-off utility functions into a more
    central location.
    
    Test Plan:
    ClearAuxTags is covered by the bio-mark-duplicates test.  GetStrand
    will be covered by the bio-mismatch-tags test in D28074.
    
    Reviewers: ayip
    
    Reviewed By: ayip
    
    Subscribers: cchang
    
    Differential Revision: https://phabricator.grailbio.com/D28247
    
    fbshipit-source-id: d68aa1d

--- encoding/bam/util.go
+++ encoding/bam/util.go
@@ -6,3 +6,63 @@ import "github.com/grailbio/hts/sam"
 func HasNoMappedMate(record *sam.Record) bool {
 	return (record.Flags&sam.Paired) == 0 || (record.Flags&sam.MateUnmapped) != 0
 }
+
+// ClearAuxTags removes all instances of the tags in tagsToRemove[] from r.
+// (Current implementation is not designed for very large len(tagsToRemove); at
+// some point map lookups become better.)
+func ClearAuxTags(r *sam.Record, tagsToRemove []sam.Tag) {
+	found := 0
+	for i := range r.AuxFields {
+		tag := r.AuxFields[i].Tag()
+		for _, toRemove := range tagsToRemove {
+			if tag == toRemove {
+				found++
+			}
+		}
+	}
+
+	if found > 0 {
+		newAux := make([]sam.Aux, len(r.AuxFields)-found)
+		size := 0
+		for i := range r.AuxFields {
+			tag := r.AuxFields[i].Tag()
+			keepAux := true
+			for _, toRemove := range tagsToRemove {
+				if tag == toRemove {
+					keepAux = false
+					break
+				}
+			}
+			if keepAux {
+				newAux[size] = r.AuxFields[i]
+				size++
+			}
+		}
+		r.AuxFields = newAux
+	}
+}
+
+type StrandType int
+
+const (
+	// StrandNone denotes an unmapped read.
+	StrandNone StrandType = iota
+	// StrandFwd denotes a read mapped to the R1+ strand.
+	StrandFwd
+	// StrandRev denotes a read mapped to the R1- strand.
+	StrandRev
+)
+
+// GetStrand returns whether the current read is mapped to the R1+ strand, the
+// R1- strand, or neither, in a manner that ignores all flags associated with
+// the other read end.
+func GetStrand(flags sam.Flags) StrandType {
+	maskedFlag := flags & (sam.Unmapped | sam.Reverse | sam.Read1 | sam.Read2)
+	if (maskedFlag == sam.Read1) || (maskedFlag == (sam.Reverse | sam.Read2)) {
+		return StrandFwd
+	}
+	if (maskedFlag == sam.Read2) || (maskedFlag == (sam.Reverse | sam.Read1)) {
+		return StrandRev
+	}
+	return StrandNone
+}
