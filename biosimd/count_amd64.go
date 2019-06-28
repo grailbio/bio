grailbio/base

base/simd: define NibbleLookupTable in preparation for AVX2 support
Summary:
The SIMD parallel-nibble-lookup functions had a [16]byte-based
interface that doesn't generalize properly to AVX2/AVX512.  This
revision introduces the necessary NibbleLookupTable type and
initialization function for these functions to use
_mm256_shuffle_epi8()/_mm512_shuffle_epi8() in the future when
AVX2/AVX512 support is detected at runtime, and updates all callers to
use the new interface.

(Current plan is to postpone actual AVX2 detection and exploitation
till after the next pipeline release, to reduce risk.)

Test Plan:
Existing unit tests for base/simd, bio/biosimd, and all modified
callers pass.  There was no regression in base/simd benchmarks.

Reviewers: ysaito, sgross

Reviewed By: ysaito

Subscribers: cchang

Differential Revision: https://phabricator.grailbio.com/D29420

fbshipit-source-id: 55126cd

simd/count_amd64.go
@@ -31,15 +31,15 @@ func count2BytesSSE41Asm(src unsafe.Pointer, val1, val2, nByte int) int
 func count3BytesSSE41Asm(src unsafe.Pointer, val1, val2, val3, nByte int) int
 
 //go:noescape
-func countNibblesInSetSSE41Asm(src unsafe.Pointer, tablePtr *[16]byte, nByte int) int
+func countNibblesInSetSSE41Asm(src unsafe.Pointer, tablePtr *NibbleLookupTable, nByte int) int
 
-func countNibblesInTwoSetsSSE41Asm(cnt2Ptr *int, src unsafe.Pointer, table1Ptr, table2Ptr *[16]byte, nByte int) int
-
-//go:noescape
-func countUnpackedNibblesInSetSSE41Asm(src unsafe.Pointer, tablePtr *[16]byte, nByte int) int
+func countNibblesInTwoSetsSSE41Asm(cnt2Ptr *int, src unsafe.Pointer, table1Ptr, table2Ptr *NibbleLookupTable, nByte int) int
 
 //go:noescape
-func countUnpackedNibblesInTwoSetsSSE41Asm(cnt2Ptr *int, src unsafe.Pointer, table1Ptr, table2Ptr *[16]byte, nByte int) int
+func countUnpackedNibblesInSetSSE41Asm(src unsafe.Pointer, tablePtr *NibbleLookupTable, nByte int) int
+
+//go:noescape
+func countUnpackedNibblesInTwoSetsSSE41Asm(cnt2Ptr *int, src unsafe.Pointer, table1Ptr, table2Ptr *NibbleLookupTable, nByte int) int
 
 //go:noescape
 func accumulate8SSE41Asm(src unsafe.Pointer, nByte int) int
@@ -209,12 +209,12 @@ func Count3Bytes(src []byte, val1, val2, val3 byte) int {
 //
 // WARNING: This function does not validate the table.  It may return a garbage
 // result on invalid input.  (However, it won't corrupt memory.)
-func CountNibblesInSet(src []byte, tablePtr *[16]byte) int {
+func CountNibblesInSet(src []byte, tablePtr *NibbleLookupTable) int {
 	nSrcByte := len(src)
 	if nSrcByte < 16 {
 		cnt := 0
 		for _, srcByte := range src {
-			cnt += int(tablePtr[srcByte&15] + tablePtr[srcByte>>4])
+			cnt += int(tablePtr.Get(srcByte&15) + tablePtr.Get(srcByte>>4))
 		}
 		return cnt
 	}
@@ -228,7 +228,7 @@ func CountNibblesInSet(src []byte, tablePtr *[16]byte) int {
 //
 // WARNING: This function does not validate the tables.  It may crash or return
 // garbage results on invalid input.  (However, it won't corrupt memory.)
-func CountNibblesInTwoSets(src []byte, table1Ptr, table2Ptr *[16]byte) (int, int) {
+func CountNibblesInTwoSets(src []byte, table1Ptr, table2Ptr *NibbleLookupTable) (int, int) {
 	nSrcByte := len(src)
 	cnt2 := 0
 	if nSrcByte < 16 {
@@ -236,8 +236,8 @@ func CountNibblesInTwoSets(src []byte, table1Ptr, table2Ptr *[16]byte) (int, int
 		for _, srcByte := range src {
 			lowBits := srcByte & 15
 			highBits := srcByte >> 4
-			cnt1 += int(table1Ptr[lowBits] + table1Ptr[highBits])
-			cnt2 += int(table2Ptr[lowBits] + table2Ptr[highBits])
+			cnt1 += int(table1Ptr.Get(lowBits) + table1Ptr.Get(highBits))
+			cnt2 += int(table2Ptr.Get(lowBits) + table2Ptr.Get(highBits))
 		}
 		return cnt1, cnt2
 	}
@@ -252,12 +252,12 @@ func CountNibblesInTwoSets(src []byte, table1Ptr, table2Ptr *[16]byte) (int, int
 //
 // WARNING: This function does not validate the table.  It may crash or return
 // a garbage result on invalid input.  (However, it won't corrupt memory.)
-func CountUnpackedNibblesInSet(src []byte, tablePtr *[16]byte) int {
+func CountUnpackedNibblesInSet(src []byte, tablePtr *NibbleLookupTable) int {
 	nSrcByte := len(src)
 	if nSrcByte < 16 {
 		cnt := 0
 		for _, srcByte := range src {
-			cnt += int(tablePtr[srcByte])
+			cnt += int(tablePtr.Get(srcByte))
 		}
 		return cnt
 	}
@@ -272,7 +272,7 @@ func CountUnpackedNibblesInSet(src []byte, tablePtr *[16]byte) int {
 //
 // WARNING: This function does not validate the tables.  It may crash or return
 // garbage results on invalid input.  (However, it won't corrupt memory.)
-func CountUnpackedNibblesInTwoSets(src []byte, table1Ptr, table2Ptr *[16]byte) (int, int) {
+func CountUnpackedNibblesInTwoSets(src []byte, table1Ptr, table2Ptr *NibbleLookupTable) (int, int) {
 	// Building this out now so that biosimd.PackedSeqCountTwo is not a valid
 	// reason to stick to packed .bam seq[] representation.
 	nSrcByte := len(src)
@@ -280,8 +280,8 @@ func CountUnpackedNibblesInTwoSets(src []byte, table1Ptr, table2Ptr *[16]byte) (
 	if nSrcByte < 16 {
 		cnt1 := 0
 		for _, srcByte := range src {
-			cnt1 += int(table1Ptr[srcByte])
-			cnt2 += int(table2Ptr[srcByte])
+			cnt1 += int(table1Ptr.Get(srcByte))
+			cnt2 += int(table2Ptr.Get(srcByte))
 		}
 		return cnt1, cnt2
 	}

grailbio/bio

biosimd/count_amd64.go
@@ -14,10 +14,10 @@ import (
 )
 
 //go:linkname countNibblesInSetSSE41Asm github.com/grailbio/base/simd.countNibblesInSetSSE41Asm
-func countNibblesInSetSSE41Asm(src unsafe.Pointer, tablePtr *[16]byte, nByte int) int
+func countNibblesInSetSSE41Asm(src unsafe.Pointer, tablePtr *NibbleLookupTable, nByte int) int
 
 //go:linkname countNibblesInTwoSetsSSE41Asm github.com/grailbio/base/simd.countNibblesInTwoSetsSSE41Asm
-func countNibblesInTwoSetsSSE41Asm(cnt2Ptr *int, src unsafe.Pointer, table1Ptr, table2Ptr *[16]byte, nByte int) int
+func countNibblesInTwoSetsSSE41Asm(cnt2Ptr *int, src unsafe.Pointer, table1Ptr, table2Ptr *NibbleLookupTable, nByte int) int
 
 // It's generally cleaner to unpack first, and then use base/simd's byte
 // counting functions.  However, we have some preexisting code which doesn't
@@ -36,14 +36,14 @@ func countNibblesInTwoSetsSSE41Asm(cnt2Ptr *int, src unsafe.Pointer, table1Ptr, 
 // WARNING: This function does not validate the table, startPos, or endPos.  It
 // may crash or return a garbage result on invalid input.  (However, it won't
 // corrupt memory.)
-func PackedSeqCount(seq4 []byte, tablePtr *[16]byte, startPos, endPos int) int {
+func PackedSeqCount(seq4 []byte, tablePtr *NibbleLookupTable, startPos, endPos int) int {
 	if endPos <= startPos {
 		return 0
 	}
 	startOffset := startPos >> 1
 	cnt := 0
 	if startPos&1 == 1 {
-		cnt = int(tablePtr[seq4[startOffset]&15])
+		cnt = int(tablePtr.Get(seq4[startOffset] & 15))
 		startOffset++
 	}
 	endOffset := endPos >> 1
@@ -51,20 +51,20 @@ func PackedSeqCount(seq4 []byte, tablePtr *[16]byte, startPos, endPos int) int {
 	if nByte < 16 {
 		for pos := startOffset; pos < endOffset; pos++ {
 			seq4Byte := seq4[pos]
-			cnt += int(tablePtr[seq4Byte&15] + tablePtr[seq4Byte>>4])
+			cnt += int(tablePtr.Get(seq4Byte&15) + tablePtr.Get(seq4Byte>>4))
 		}
 	} else {
 		seq4Header := (*reflect.SliceHeader)(unsafe.Pointer(&seq4))
 		cnt += countNibblesInSetSSE41Asm(unsafe.Pointer(seq4Header.Data+uintptr(startOffset)), tablePtr, nByte)
 	}
 	if endPos&1 == 1 {
-		cnt += int(tablePtr[seq4[endOffset]>>4])
+		cnt += int(tablePtr.Get(seq4[endOffset] >> 4))
 	}
 	return cnt
 }
 
 // PackedSeqCountTwo counts the number of .bam base codes in positions
-// startPos..(endPos - 1) of seq4 in the given two set, where seq4 is in .bam
+// startPos..(endPos - 1) of seq4 in the given two sets, where seq4 is in .bam
 // packed 4-bit big-endian format.
 //
 // The sets must be represented as table[x] == 1 when code x is in the set, and
@@ -73,7 +73,7 @@ func PackedSeqCount(seq4 []byte, tablePtr *[16]byte, startPos, endPos int) int {
 // WARNING: This function does not validate the tables, startPos, or endPos.
 // It may crash or return garbage results on invalid input.  (However, it won't
 // corrupt memory.)
-func PackedSeqCountTwo(seq4 []byte, table1Ptr, table2Ptr *[16]byte, startPos, endPos int) (int, int) {
+func PackedSeqCountTwo(seq4 []byte, table1Ptr, table2Ptr *NibbleLookupTable, startPos, endPos int) (int, int) {
 	if endPos <= startPos {
 		return 0, 0
 	}
@@ -81,8 +81,8 @@ func PackedSeqCountTwo(seq4 []byte, table1Ptr, table2Ptr *[16]byte, startPos, en
 	cnt1, cnt2 := 0, 0
 	if startPos&1 == 1 {
 		lowBits := seq4[startOffset] & 15
-		cnt1 = int(table1Ptr[lowBits])
-		cnt2 = int(table2Ptr[lowBits])
+		cnt1 = int(table1Ptr.Get(lowBits))
+		cnt2 = int(table2Ptr.Get(lowBits))
 		startOffset++
 	}
 	endOffset := endPos >> 1
@@ -92,8 +92,8 @@ func PackedSeqCountTwo(seq4 []byte, table1Ptr, table2Ptr *[16]byte, startPos, en
 			seq4Byte := seq4[pos]
 			lowBits := seq4Byte & 15
 			highBits := seq4Byte >> 4
-			cnt1 += int(table1Ptr[lowBits] + table1Ptr[highBits])
-			cnt2 += int(table2Ptr[lowBits] + table2Ptr[highBits])
+			cnt1 += int(table1Ptr.Get(lowBits) + table1Ptr.Get(highBits))
+			cnt2 += int(table2Ptr.Get(lowBits) + table2Ptr.Get(highBits))
 		}
 	} else {
 		seq4Header := (*reflect.SliceHeader)(unsafe.Pointer(&seq4))
@@ -101,8 +101,8 @@ func PackedSeqCountTwo(seq4 []byte, table1Ptr, table2Ptr *[16]byte, startPos, en
 	}
 	if endPos&1 == 1 {
 		highBits := seq4[endOffset] >> 4
-		cnt1 += int(table1Ptr[highBits])
-		cnt2 += int(table2Ptr[highBits])
+		cnt1 += int(table1Ptr.Get(highBits))
+		cnt2 += int(table2Ptr.Get(highBits))
 	}
 	return cnt1, cnt2
 }